9fe2a3396dd9123d6d9f4a1a10b257e873e7bafd
ddayin <ddayin@hotmail.com>
2025-02-12 23:06:54 +0900

    add scripts

---
+++ Assets/Scripts/Managers/PlayScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DivisionLike
+{
+    /// <summary>
+    /// 플레이 씬
+    /// </summary>
+    public class PlayScene : MonoBehaviour
+    {
+        private void Awake()
+        {
+        }
+    }
+}
